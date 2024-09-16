2631bbbf0da2d2b67c6b3e26b88be7b01a40a457
Samuel Bright Bakah <samuelbrightsk@gmail.com>
2024-09-16 11:39:21 +0000

    new registrstion forms

---
+++ script/register.js
@@ -0,0 +1,11 @@
+// selct form by id
+
+const registerForm = document.getElementById('register-from');
+// handle form submit event
+registerForm.onsubmitv= function() {
+    //  collect username
+    //  collect password
+    //  collect password
+    //  save user  information
+    //  send confirmation email
+}
